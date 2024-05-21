dc6301308f05384126f9e663b071c1e7961f375a
Vinicius de Liz <vinicius.deliz@gmail.com>
2024-05-20 21:25:30 -0300

    feat: include ApiProperty in create-item.dto.ts so Swagger shows up properties

--- src/infraestructure/dto/create-item.dto.ts
+++ src/infraestructure/dto/create-item.dto.ts
@@ -1,25 +1,32 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { ItemCategory } from '@prisma/client';
 import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 import { ItemModel } from '../../domain/models/items.model';
 
 export class CreateItemDTO implements ItemModel {
-  imageUrl?: string;
   id: number;
+  createdAt: Date;
+  updatedAt: Date;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsNotEmpty()
   price: number;
 
-  createdAt: Date;
-
+  @ApiProperty()
   @IsEnum(ItemCategory)
   category: ItemCategory;
+
+  @ApiPropertyOptional()
+  imageUrl?: string;
 }
